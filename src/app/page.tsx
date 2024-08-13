c3c3d9fdce30e2053d7f2670fd73cf2b51a38a5c
Robert Crocker <rcrocker13@gmail.com>
2024-08-13 07:03:17 -0500

    'Added @radix-ui/react-switch to dependencies and created Switch component. This enhances accessibility and user interactivity.'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { QUIZ_DATA } from "@/data/data";
 import { IconWrapper } from "@/components/icon-wrapper";
 import { useState } from "react";
 import { ButtonGroup, ButtonGroupItem } from "@/components/ui/button-group";
-import HTMLIcon from "@/components/icons/HTMLIcon";
-import { cn } from "@/lib/utils";
-
-// const quizes = QUIZ_DATA.quizzes.map((quiz) => quiz.title);
-// const htmlQuestions = QUIZ_DATA.quizzes.filter(
-//   (quiz) => quiz.title === "HTML",
-// )[0];
+import SunLightIcon from "@/components/icons/SunLightIcon";
+import MoonLightIcon from "@/components/icons/MoonLightIcon";
+import SunDarkIcon from "@/components/icons/SunDarkIcon";
+import MoonDarkIcon from "@/components/icons/MoonDarkIcon";
 
 const question = {
   question: "",
@@ -28,9 +26,15 @@ export default function Home() {
   const [currentChoice, setCurrentChoice] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
   const [quizState, setQuizState] = useState(DEFAULT_QUIZ_STATE);
+  const currentScore = quizState.reduce((acc, curr) => {
+    const point = curr.isAnsweredCorrectly ? 1 : 0;
+    return acc + point;
+  }, 0);
 
+  // console.log("currentPage", currentPage);
+  console.log("quizSelection", quizSelection);
   console.log("quizState", quizState);
-  console.log("currentChoice", currentChoice);
+  // console.log("currentChoice", currentChoice);
 
   const handleQuizSelection = (value: string) => {
     const selectedQuizData = QUIZ_DATA.quizzes.filter((quiz) => {
@@ -135,8 +139,25 @@ export default function Home() {
           </div>
         </section>
       )}
-      {currentPage !== 0 && (
+      {currentPage !== 0 && currentPage !== 10 && (
         <section className="flex w-full max-w-[1160px] flex-col gap-8">
+          <header className="mb-14 flex items-center justify-between">
+            <div className="flex items-center gap-6 text-[28px] font-medium">
+              <IconWrapper
+                iconName={
+                  quizSelection?.title === "Javascript"
+                    ? "JS"
+                    : quizSelection?.title
+                }
+              />{" "}
+              <span>{quizSelection?.title}</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <SunDarkIcon />
+              <Switch />
+              <MoonDarkIcon />
+            </div>
+          </header>
           <section className="flex w-full justify-between">
             <section className="flex w-[465px] flex-col justify-between">
               <section className="flex flex-col gap-6">
@@ -180,6 +201,43 @@ export default function Home() {
               }
             >
               Submit Answer
+            </Button>
+          </div>
+        </section>
+      )}
+      {currentPage === 10 && (
+        <section className="flex w-full justify-between">
+          <section className="flex flex-col gap-12">
+            <section className="flex flex-col gap-0 text-[64px] leading-none text-fem-dark-navy">
+              <div className="font-light">Quiz completed</div>
+              <div className="font-medium">You scored...</div>
+            </section>
+          </section>
+          <div className="flex w-[564px] flex-col gap-6">
+            <div className="flex w-full flex-col items-center gap-10 rounded-3xl bg-fem-pure-white p-12 text-fem-dark-navy shadow-[0px_16px_40px_0px_#8FA0C124]">
+              <div className="flex items-center gap-6 text-[28px] font-medium">
+                <IconWrapper
+                  iconName={
+                    quizSelection?.title === "Javascript"
+                      ? "JS"
+                      : quizSelection?.title
+                  }
+                />{" "}
+                <span>{quizSelection?.title}</span>
+              </div>
+              <div className="flex flex-col items-center gap-4">
+                <div className="text-[144px] font-medium leading-none">
+                  {currentScore}
+                </div>
+                <div className="text-[24px] text-fem-grey-navy">out of 10</div>
+              </div>
+            </div>
+            <Button
+              variant="default"
+              className="w-[564px]"
+              onClick={() => setCurrentPage(0)}
+            >
+              Play Again
             </Button>
           </div>
         </section>
